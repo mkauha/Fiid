c8aca1040559842a77a690bb75b2c1832eaffe0a
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-05-05 19:26:05 +0300

    Add routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { FormbuilderComponent } from './formbuilder/formbuilder.component';
+import { GeneratedFormComponent } from './generated-form/generated-form.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'new', component: FormbuilderComponent },
+  { path: 'form', component: GeneratedFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
