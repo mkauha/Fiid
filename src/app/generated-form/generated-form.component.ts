4ab7be9d3313d8658153dc1e00ca690cfe9ee8d1
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-05-11 20:36:16 +0300

    Add button to generate dummy form.

--- src/app/generated-form/generated-form.component.ts
+++ src/app/generated-form/generated-form.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { QuestionControlService } from '../question-control.service';
 import { QuestionBase } from '../forms/question-base';
+import { TextboxQuestion } from '../forms/question-textbox';
+import { TextareaQuestion } from '../forms/question-textarea';
+import { RadioQuestion } from '../forms/question-radio';
 
 @Component({
   selector: 'app-generated-form',
@@ -12,12 +15,36 @@ import { QuestionBase } from '../forms/question-base';
 export class GeneratedFormComponent implements OnInit {
   form: FormGroup;
   questions: QuestionBase[] = [];
+  formTitle = ' ';
 
   constructor(private qcs: QuestionControlService) { }
 
   ngOnInit(): void {
     this.questions = this.qcs.getQuestions();
+    this.formTitle = this.qcs.getFormTitle();
     this.form = this.qcs.getFormGroup();
   }
+
+  generateTextForm() {
+    this.formTitle = 'Test form';
+    this.questions.push(new TextboxQuestion({questionID: 1, key: 'firstname', label: 'Firstname', textboxtype: 'string', required: true}));
+    this.questions.push(new TextboxQuestion({questionID: 2, key: 'lastname', label: 'Lastname', textboxtype: 'string', required: true}));
+    this.questions.push(new TextareaQuestion({questionID: 3, key: 'story', label: 'Story of your life', rows: 10, required: true}));
+    this.questions.push(new RadioQuestion({questionID: 4, key: 'happiness', label: 'Happy?', optionAmount: 3, required: true}));
+    this.form = this.generateTestFormGroup();
+  }
+
+  generateTestFormGroup(): FormGroup {
+    const group: any = {};
+
+    for (const question of this.questions) {
+        if (question.required) {
+            group[question.key] = new FormControl('', Validators.required);
+        } else {
+            group[question.key] = new FormControl();
+        }
+    }
+    return new FormGroup(group);
+}
 
 }
