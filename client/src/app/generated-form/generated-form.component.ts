fd990dbf9f3b05342bc20c06ca3d127707d2cbc3
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-05-16 20:35:04 +0300

    Fetch form data from url params.

--- client/src/app/generated-form/generated-form.component.ts
+++ client/src/app/generated-form/generated-form.component.ts
@@ -2,12 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { QuestionControlService } from '../question-control.service';
 import { QuestionBase } from '../forms/question-base';
-import { TextboxQuestion } from '../forms/question-textbox';
-import { TextareaQuestion } from '../forms/question-textarea';
-import { RadioQuestion } from '../forms/question-radio';
-import { EmojiQuestion } from '../forms/question-emoji';
 import { HttpService } from '../http.service';
 import { GeneratedForm } from '../forms/generated-form';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-generated-form',
@@ -18,18 +16,28 @@ import { GeneratedForm } from '../forms/generated-form';
 export class GeneratedFormComponent implements OnInit {
   formGroup: FormGroup;
   questions: QuestionBase[] = [];
+  formUUID = '';
   formTitle = ' ';
   formDate = ' ';
+  baseUrl = 'http://localhost:3000/forms/';
   formUrl = 'http://localhost:3000/forms/0ae34a35-d281-4867-89c2-1e2d77d747b6';
 
-  constructor(private qcs: QuestionControlService, private httpService: HttpService) { }
+  public showUrlInput = false;
+
+  constructor(private qcs: QuestionControlService,
+              private httpService: HttpService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
-/*     this.questions = this.qcs.getQuestions();
-    this.formTitle = this.qcs.getFormTitle();
-    this.form = this.qcs.getFormGroup(); */
-    //this.generateTextForm();
-    //this.onFindForm();
+    this.route.queryParams.subscribe(params => {
+      console.log(params);
+      this.formUrl = `${this.baseUrl}${params.id}`;
+      console.log(this.formUrl);
+
+      this.fetchForm(this.formUrl);
+
+    });
   }
 
   onSubmit() {
@@ -38,14 +46,33 @@ export class GeneratedFormComponent implements OnInit {
     // this.httpService.postForm(this.form.value);
   }
 
-  onFindForm() {
+  fetchForm(url: string) {
     this.httpService.fetchForm((result) => {
+      if (result === null) {
+        this.showUrlInput = true;
+      }
       this.questions = JSON.parse(JSON.stringify(result.form.questions));
       this.formTitle = JSON.parse(JSON.stringify(result.form.title));
       this.formDate = JSON.parse(JSON.stringify(result.form.date));
       this.qcs.saveForm(new GeneratedForm(this.formTitle, new Date(this.formDate), this.questions));
       this.formGroup = this.qcs.getFormGroup();
-    }, this.formUrl);
+
+
+    }, url);
+
+  }
+
+  onFindForm() {
+
+    const urlArr = this.formUrl.split('/');
+    console.log(urlArr)
+    const paramArr = urlArr[3].split('=');
+    console.log(paramArr)
+    this.formUUID = paramArr[1];
+    this.formUrl = `${this.baseUrl}${this.formUUID}`;
+    this.router.navigate(['/form'], { queryParams: { id: this.formUUID } });
+
+    this.fetchForm(this.formUrl);
   }
 
 /*   generateTextForm() {
