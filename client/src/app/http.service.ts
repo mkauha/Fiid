f9ec97692059a9a1dcd7e3b49dd1984eddbbb9d1
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-05-16 16:21:12 +0300

    Implement post and get for generated forms.

--- client/src/app/http.service.ts
+++ client/src/app/http.service.ts
@@ -2,25 +2,39 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
+import { GeneratedForm } from './forms/generated-form';
+import { Form } from './forms/form';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-  private fetchUrl = 'https://swapi.co/api/planets/';
-  private postUrl = 'https://my-json-server.typicode.com/mkauha/JSON-server-demo/blogposts';
-  
+  private postUrl = 'http://localhost:3000/forms/';
+
   constructor(private http: HttpClient) { }
 
-  fetchForm(callBackFunction: (result: Object[]) => void): void {
-    this.http.get(this.fetchUrl).subscribe(jsonObject => {
+  fetchForm(callBackFunction: (result: any) => void, fetchUrl: string): void {
+    this.http.get<any>(fetchUrl).subscribe(jsonObject => {
       console.log(jsonObject);
+      callBackFunction(jsonObject);
     });
   }
 
   postForm(form: object) {
-    this.http.post<any>(this.postUrl, form).subscribe(data => {
+    console.log(form);
+/*     this.http.post<any>(this.postUrl, form).subscribe(data => {
       console.log(data);
+    }); */
+  }
+
+  postGeneratedForm(generatedForm: GeneratedForm) {
+    let postedForm = {id: 1, form: generatedForm};
+    this.http.post<GeneratedForm>(this.postUrl, postedForm, {observe: 'response'}).subscribe(response => {
+      console.log(response);
     });
   }
+}
+
+interface GeneratedFormResponse {
+  results: Form;
 }
