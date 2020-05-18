c5692600805f2472df42b4d12e9e0dbf226e7f44
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-05-18 19:50:15 +0300

    Remove unnecessary code.

--- client/src/app/question-control.service.ts
+++ client/src/app/question-control.service.ts
@@ -14,13 +14,6 @@ export class QuestionControlService {
         this.form = form;
         this.questions = form.questions;
         this.formTitle = form.title;
-
-/*         console.log('QCS');
-        console.log(form);
-        console.log(form.questions);
-        console.log(this.questions);
-        console.log(form.title);
-        console.log(this.formTitle); */
     }
 
 
@@ -51,7 +44,6 @@ export class QuestionControlService {
                 group[question.key] = new FormControl();
             }
         }
-        // return new FormGroup({name: new FormControl(), age: new FormControl()})
         return new FormGroup(group);
     }
 }
