e1b3f4eeb50cbbc5291f1f2b75415cf76ba36100
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-05-18 16:16:12 +0300

    Create results component.

--- client/src/app/results/results.component.ts
+++ client/src/app/results/results.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpService } from '../http.service';
+import { QuestionBase } from '../forms/question-base';
 
 @Component({
   selector: 'app-results',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./results.component.css']
 })
 export class ResultsComponent implements OnInit {
+  formUUID = '';
+  formTitle = ' ';
+  formDate = ' ';
+  questions: QuestionBase[] = [];
+  formResults = [];
+  baseUrl = 'http://localhost:3000/forms/';
+  formUrl = 'http://localhost:3000/forms/0ae34a35-d281-4867-89c2-1e2d77d747b6';
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private httpService: HttpService) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      console.log(params);
+      this.formUUID = params.id;
+      console.log(`Results fetch id: ${this.formUUID}`);
+      this.formUrl = `${this.baseUrl}${params.id}`;
+      console.log(`Results fetch url: ${this.formUrl}`);
+
+      this.fetchForm(this.formUrl);
+
+    });
   }
+
+  fetchForm(url: string) {
+    this.httpService.fetchForm((result) => {
+      this.questions = JSON.parse(JSON.stringify(result.form.questions));
+      this.formTitle = JSON.parse(JSON.stringify(result.form.title));
+      this.formDate = JSON.parse(JSON.stringify(result.form.date));
+
+      for (const question of this.questions) {
+        this.formResults.push(question.results);
+      }
+      console.log(this.formResults);
+    }, url);
+
+  }
+
 
 }
