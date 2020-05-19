51a18e1efb45e5b49cca83e4c0ca34cc16104703
Miko Kauhanen <miko.kauhanen@gmail.com>
2020-05-19 19:40:03 +0300

    Allow input of from url and id.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import { environment } from 'src/environments/environment';
 export class HomeComponent implements OnInit {
   public showFormResultsInput = false;
   public baseApiUrl = environment.baseApiUrl;
+  public baseClientUrl = environment.baseClientUrl;
   public formUrl = `${this.baseApiUrl}0ae34a35-d281-4867-89c2-1e2d77d747b6`;
   public formResultsUUID = '';
   public formUUID = '';
@@ -20,10 +21,20 @@ export class HomeComponent implements OnInit {
   }
 
   onGoToResults() {
+    if (this.formResultsUUID.includes(this.baseClientUrl)) {
+      const urlArr = this.formResultsUUID.split('/');
+      const paramArr = urlArr[3].split('=');
+      this.formResultsUUID = paramArr[1];
+    }
     this.router.navigate(['/results'], { queryParams: { id: this.formResultsUUID } });
   }
 
   onGoToForm() {
+    if (this.formUUID.includes(this.baseClientUrl)) {
+      const urlArr = this.formUUID.split('/');
+      const paramArr = urlArr[3].split('=');
+      this.formUUID = paramArr[1];
+    }
     this.router.navigate(['/form'], { queryParams: { id: this.formUUID } });
   }
 
